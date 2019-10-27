d2d4bd73d4897e16323b1149bebf0341f0803832
mohamed <mohamed.kalmoua@gmail.com>
2019-10-27 13:29:37 +0100

    Initial commit of projects.

---
+++ CodeSnippets/CodeSnippets/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
